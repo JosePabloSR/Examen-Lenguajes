04f48dab3d41f4d935caf0bbdd720b1004d44181
JosePabloSR <josepablosolisrodriguez@gmail.com>
2024-05-25 10:27:13 -0600

    Mejoras en el diseño web

--- src/app/user/mygithub/profile.js
+++ src/app/user/mygithub/profile.js
@@ -15,12 +15,12 @@ export default function Profile(props) {
           />
           <ul className="ml-5">
             <li>
-              <h2 className="font-bold text-xl">{props.owner.login}</h2>
+              <h2 className="bg-red-700 py-1 px-2 rounded-full font-bold text-xl">{props.owner.login}</h2>
             </li>
             <div>
-              <p className="mr-2">{props.name}</p>
+              <p className=" mr-2">{props.name}</p>
               {props.private ? (
-                <p className="bg-rose-700 py-1 px-2 rounded-lg shadow text-white text-xs inline-block opacity-75">
+                <p className="bg-red-700 py-1 px-2 rounded-lg shadow text-white text-xs inline-block opacity-75">
                   Privado
                 </p>
               ) : (
@@ -33,7 +33,7 @@ export default function Profile(props) {
         </div>
 
         <div>
-          <p className="mt-5">
+          <p className="bg-red-700 rounded-lg p-1 mt-1">
             Este repositorio fue creado el {" "}
             {format(new Date(props.created_at), "dd MMMM yyyy")} por {" "}
             {props.owner.login}
@@ -42,7 +42,7 @@ export default function Profile(props) {
 
         <div className="mt-5 flex items-center justify-between text-right">
           <a
-            className="underline text-sm"
+            className="bg-blue-800 rounded-full p-3 mt-1  text-sm"
             href={props.html_url}
             target="_blank"
             rel="noreferrer"
@@ -57,9 +57,9 @@ export default function Profile(props) {
 
         <div className="flex items-center justify-between flex-wrap mt-5">
           <ul className="text-xs flex items-center justify-start">
-            <li className="py-1 px-2 text-white bg-emerald-700 opacity-75 rounded-lg shadow inline-block mr-2">
-              {props.language}
-            </li>
+          <li className="py-1 px-2 text-white bg-red-700 opacity-75 rounded-lg shadow inline-block mr-2">
+          {props.language}
+      </li>
 
             {props.topics &&
               props.topics.map((topic, index) => (
